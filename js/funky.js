c5d27da558354c7bb7f83b6b9f356cbd4aeb9e20
Paul Hirsch <paul@santasoft.com>
2014-04-09 12:53:29 -0500

    tweaking to get the online sched

--- js/funky.js
+++ js/funky.js
@@ -35,3 +35,51 @@
 				}
 			});
 		});
+
+
+		$("#wakaSchedule").click(function() {
+			console.log("getting the data from server, foo");
+			$.ajax( {url : "http://www.wakarusa.com/stage-schedule-mobile/",
+				success : function(data) {
+					console.log("Got me some data for the wakaSchedule div");
+					$("#liveSchedule").html(data);
+					//console.log(data);
+				},
+				error : function() {
+					console.log("ERROR");
+					$("#liveSchedule").html("Missing Schedule Data");
+					//console.log(data);
+				}, 
+				dataType: "html"
+			});
+		});
+
+
+
+		$("#wakaCampsite").click(function() {
+			console.log("getting geolocation from phone");
+			var onSuccess = function(position) {
+				console.log("got some geolocation");
+				$("#yourLocation b").html(
+						 ('Latitude: '          + position.coords.latitude          + '\n' +
+				          'Longitude: '         + position.coords.longitude         + '\n' +
+				          'Altitude: '          + position.coords.altitude          + '\n' +
+				          'Accuracy: '          + position.coords.accuracy          + '\n' +
+				          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+				          'Heading: '           + position.coords.heading           + '\n' +
+				          'Speed: '             + position.coords.speed             + '\n' +
+				          'Timestamp: '         + position.timestamp                + '\n'
+				          )
+				);
+				
+				// onError Callback receives a PositionError object
+				//
+				function onError(error) {
+				    alert('code: '    + error.code    + '\n' +
+				          'message: ' + error.message + '\n');
+				    console.log("geoloc fetch error: " + error.message + "");
+				}
+				
+				navigator.geolocation.getCurrentPosition(onSuccess, onError);
+			}
+		});
